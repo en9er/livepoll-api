e3486526079249a31c54594d73c7dd5d5f2ddbad
dkhabibullin <dkhabibullin@cloudlinux.com>
2023-09-18 22:13:01 +0600

    Init structure of fastAPI app

---
+++ app/main.py
@@ -0,0 +1,52 @@
+import logging.config
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+    "http://localhost:4201",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
